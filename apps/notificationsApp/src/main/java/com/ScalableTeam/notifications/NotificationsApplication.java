581c97d0682a2e51064849f5beee02a86faa6915
Maria Maged <mariamaged988@gmail.com>
2022-05-12 01:16:05 +0200

    Base Module for Rabbit Message Queueu

--- apps/notificationsApp/src/main/java/com/ScalableTeam/notifications/NotificationsApplication.java
+++ apps/notificationsApp/src/main/java/com/ScalableTeam/notifications/NotificationsApplication.java
@@ -1,12 +1,41 @@
 package com.ScalableTeam.notifications;
 
+import com.ScalableTeam.amqp.RabbitMQMessageProducer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.ScalableTeam.notifications",
+                "com.ScalableTeam.amqp",
+        }
+)
 public class NotificationsApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationsApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(
+            RabbitMQMessageProducer producer) {
+        return args -> {
+            producer.publish(
+                    new Person("Maria", 23),
+                    "amq.direct",
+                    "notif");
+        };
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Person {
+    private String name;
+    private int age;
 }
