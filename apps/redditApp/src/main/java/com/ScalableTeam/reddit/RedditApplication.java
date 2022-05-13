a837a6f206d9e7a10d80c98ce995d71f05840aa0
Maria Maged <mariamaged988@gmail.com>
2022-05-13 21:10:04 +0200

    Commit configuring redditApp for service discovery

--- apps/redditApp/src/main/java/com/ScalableTeam/reddit/RedditApplication.java
+++ apps/redditApp/src/main/java/com/ScalableTeam/reddit/RedditApplication.java
@@ -3,11 +3,12 @@ package com.ScalableTeam.reddit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-
+@EnableEurekaClient
 public class RedditApplication {
 
     public static void main(String[] args) {
