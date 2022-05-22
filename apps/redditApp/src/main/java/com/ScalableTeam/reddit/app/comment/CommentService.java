6a398b045c7f9af0688e8969d6d16d90c713dae9
Maria Maged <mariamaged988@gmail.com>
2022-05-22 20:53:12 +0200

    Commit edges addition

--- apps/redditApp/src/main/java/com/ScalableTeam/reddit/app/comment/CommentService.java
+++ apps/redditApp/src/main/java/com/ScalableTeam/reddit/app/comment/CommentService.java
@@ -2,13 +2,9 @@ package com.ScalableTeam.reddit.app.comment;
 
 import com.ScalableTeam.reddit.MyCommand;
 import com.ScalableTeam.reddit.app.caching.CachingService;
-import com.ScalableTeam.reddit.app.entity.Comment;
-import com.ScalableTeam.reddit.app.entity.Post;
-import com.ScalableTeam.reddit.app.entity.User;
+import com.ScalableTeam.reddit.app.entity.*;
 
-import com.ScalableTeam.reddit.app.repository.CommentRepository;
-import com.ScalableTeam.reddit.app.repository.PostRepository;
-import com.ScalableTeam.reddit.app.repository.UserRepository;
+import com.ScalableTeam.reddit.app.repository.*;
 
 import com.ScalableTeam.reddit.config.GeneralConfig;
 import lombok.extern.slf4j.Slf4j;
@@ -39,11 +35,13 @@ public class CommentService implements MyCommand {
     private GeneralConfig generalConfig;
     @Autowired
     private CachingService cachingService;
-    //    public CommentService(PostRepository postRepository) {
-//        this.postRepository = postRepository;
-//    }
+    @Autowired
+    private CommentChildrenHierarchyRepository commentChildrenHierarchyRepository;
+    @Autowired
+    private PostCommentHierarchyRepository postCommentHierarchyRepository;
+
     @Override
-    public Post execute(Object body) throws Exception{
+    public Post execute(Object body) throws Exception {
         log.info(generalConfig.getCommands().get("comment") + "Service", body);
         try {
 //            CommentResponseForm commentResponseForm=(CommentResponseForm) body;
@@ -55,37 +53,42 @@ public class CommentService implements MyCommand {
             final Optional<Post> postParentOptional = postRepository.findById(postId);
 //            final String channelId=postRepository.getChannelOfPost(comment.getCommentParentId());
             //user has to follow the channel
-            if (postCreatorOptional.isEmpty()||postParentOptional.isEmpty()||
-            !postCreatorOptional.get().getFollowedChannels().containsKey(postParentOptional.get().getChannelId())) {
+            if (postCreatorOptional.isEmpty() || postParentOptional.isEmpty() ||
+                    !postCreatorOptional.get().getFollowedChannels().containsKey(postParentOptional.get().getChannelId())) {
                 throw new Exception();
             }
             //check if comment parent belongs to the same post
-            if (!comment.isCommentOnPost() &&
-                    !postParentOptional.get().getComments().containsKey(comment.getCommentParentId())) {
-                throw new Exception();
-
+            if (!comment.isCommentOnPost()) {
+                if (!(commentRepository.existsById(comment.getCommentParentId()) &&
+                        commentRepository.findById(comment.getCommentParentId()).get()
+                                .getPostId().equals(postId)))
+                    throw new Exception();
             }
-            Post post=postParentOptional.get();
+
+            comment.setPostId(postId);
             commentRepository.save(comment);
-            HashMap<String, Comment> hm = new HashMap<>();
+            Post post = postParentOptional.get();
+            if (!comment.isCommentOnPost()) {
+                CommentToComment commentToComment = CommentToComment.builder()
+                        .parentComment(commentRepository.findById(comment.getCommentParentId()).get())
+                        .childComment(comment)
+                        .build();
+                commentChildrenHierarchyRepository.save(commentToComment);
+            } else {
+                PostToComment postToComment = PostToComment.builder()
+                        .post(post)
+                        .comment(comment)
+                        .build();
+                postCommentHierarchyRepository.save(postToComment);
+            }
             //Comment Id is generated and set by save of commentRepository
             //Update the post and return it to update cache
-            hm.put(comment.getId(), comment);
-            if(post.getComments()==null){
-                post.setComments(hm);
-            }
-            else {
-                hm=post.getComments();
-                hm.put(comment.getId(), comment);
-                post.setComments(hm);
-            }
-            postRepository.updateFieldInPost(postId, "comments", hm);
-            if(cacheManager.getCache("postsCache").get(postId)!=null) {
-                cachingService.updatePostsCache(postId,post);
+            if (cacheManager.getCache("postsCache").get(postId) != null) {
+                cachingService.updatePostsCache(postId, post);
                 System.err.println("in cache");
             }
-            if(cacheManager.getCache("popularPostsCache").get(postId)!=null)
-                cachingService.updatePopularPostsCache(postId,post);
+            if (cacheManager.getCache("popularPostsCache").get(postId) != null)
+                cachingService.updatePopularPostsCache(postId, post);
             return post;
         } catch (Exception e) {
 
