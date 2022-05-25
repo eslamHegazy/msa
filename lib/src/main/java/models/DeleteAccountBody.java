0d651e08924a46c19d62d2ff751eff5295a053de
MedoRedo <moreda.elsayed@gmail.com>
2022-05-25 22:49:57 +0200

    Added lib module and user app module

---
+++ lib/src/main/java/models/DeleteAccountBody.java
@@ -0,0 +1,12 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DeleteAccountBody {
+    private String userId;
+}
---
+++ lib/src/main/java/models/SignUpBody.java
@@ -0,0 +1,14 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SignUpBody{
+    private String userId;
+    private String email;
+    private String password;
+}
