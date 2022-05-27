caf7c2ef7795e81210f9e1b49d647d74b4f2bd14
OmarElbaroudy <elbaroudyomar@gmail.com>
2022-05-27 13:08:36 +0200

    feat-1: search by title or channel

--- apps/redditApp/src/main/java/com/ScalableTeam/reddit/app/repository/PostRepository.java
+++ apps/redditApp/src/main/java/com/ScalableTeam/reddit/app/repository/PostRepository.java
@@ -11,14 +11,27 @@ import java.util.HashMap;
 public interface PostRepository extends ArangoRepository<Post, String> {
     @Query("FOR p IN posts FILTER p._key == @key RETURN p.channelId")
     String getChannelOfPost(@Param("key") String key);
+
     @Query("FOR p IN posts FILTER p._key > @latestReadPostId AND HAS(@followedChannels,p.channelId) LIMIT 25 RETURN p")
-    Post[] getPostsByTimeAndChannel(@Param("latestReadPostId") String latestReadPostId, @Param("followedChannels")HashMap<String,Boolean> followedChannels);
+    Post[] getPostsByTimeAndChannel(@Param("latestReadPostId") String latestReadPostId, @Param("followedChannels") HashMap<String, Boolean> followedChannels);
+
     @Query("FOR p IN posts FILTER  p._key > @latestReadPostId AND HAS(@followedUsers,p.userNameId) LIMIT 25 RETURN p")
-    Post[] getPostsByTimeAndUser(@Param("latestReadPostId") String latestReadPostId,@Param("followedUsers") HashMap<String,Boolean> followedUsers);
+    Post[] getPostsByTimeAndUser(@Param("latestReadPostId") String latestReadPostId, @Param("followedUsers") HashMap<String, Boolean> followedUsers);
+
     @Query("FOR p IN posts UPDATE {_key:@key,@fieldName:@fieldValue} IN posts")
     void updateFieldInPost(@Param("key") String key, @Param("fieldName") String fieldName, @Param("fieldValue") Object fieldValue);
-    @Query("FOR p IN posts FILTER HAS(@channelId,p.channelId)")
-    Post[] getPostsByChannel(@Param("followedChannels")String channelId);
 
+    @Query("FOR p IN posts FILTER HAS(@channelId,p.channelId)")
+    Post[] getPostsByChannel(@Param("followedChannels") String channelId);
+
+    @Query("FOR p IN posts SEARCH ANALYZER" +
+            "(p.title IN TOKENS(@title, \"text_en\"), \"text_en\")" +
+            "RETURN p")
+    Post[] getPostsByPostTitle(@Param("title") String title);
+
+    @Query("FOR p IN posts SEARCH ANALYZER" +
+            "(STARTS_WITH(p.channelId, LOWER(LTRIM(@id))) OR " +
+            "PHRASE(p.channelId, @id), \"text_en\") RETURN p")
+    Post[] getPostsByChannelId(@Param("id") String id);
 
 }
---
+++ apps/redditApp/src/main/java/com/ScalableTeam/reddit/app/search/searchController.java
@@ -0,0 +1,49 @@
+package com.ScalableTeam.reddit.app.search;
+
+import com.ScalableTeam.amqp.Config;
+import com.ScalableTeam.amqp.RabbitMQProducer;
+import com.ScalableTeam.reddit.app.MessagePublisher;
+import com.ScalableTeam.reddit.config.GeneralConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/search")
+public class searchController extends MessagePublisher {
+    @Autowired
+    private GeneralConfig generalConfig;
+    @Autowired
+    private Config config;
+    @Autowired
+    private RabbitMQProducer rabbitMQProducer;
+
+    @GetMapping("by_title/{title}")
+    public String searchByTitle(@PathVariable String title) {
+        final String COMMAND_NAME = "searchByTitle";
+
+        String indicator = generalConfig.getCommands().get(COMMAND_NAME);
+        log.info(indicator + "Controller::Post title={}", title);
+
+        return (String) rabbitMQProducer.publishSynchronous(
+                title,
+                config.getExchange(),
+                config.getQueues().getRequest().getReddit().get(COMMAND_NAME));
+    }
+
+    @GetMapping("by_channel/{id}")
+    public String searchByChannel(@PathVariable String id) {
+        final String COMMAND_NAME = "searchByChannel";
+
+        String indicator = generalConfig.getCommands().get(COMMAND_NAME);
+        log.info(indicator + "Controller::Channel Id={}", id);
+
+        return (String) rabbitMQProducer.publishSynchronous(id,
+                config.getExchange(),
+                config.getQueues().getRequest().getReddit().get(COMMAND_NAME));
+    }
+}
