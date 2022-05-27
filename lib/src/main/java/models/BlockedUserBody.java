47eea4d199f2e2f86619d741efdf4c1a8249cb9d
AhmedAbdouz <zewail102@gmail.com>
2022-05-27 15:47:22 +0200

    added Report and block user files in user app and updated UserRepository

---
+++ lib/src/main/java/models/BlockedUserBody.java
@@ -0,0 +1,13 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BlockedUserBody {
+    private String userId;
+    private String blockedUserId;
+}
---
+++ lib/src/main/java/models/ReportUserBody.java
@@ -0,0 +1,14 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ReportUserBody {
+    private String userId;
+    private String reportedUserId;
+    private String reason;
+}
