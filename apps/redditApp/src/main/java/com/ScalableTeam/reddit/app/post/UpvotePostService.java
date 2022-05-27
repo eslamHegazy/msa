666772729c7cb46b410115c61d6149723850e3cd
Maria Maged <mariamaged988@gmail.com>
2022-05-28 00:24:57 +0200

    Commit upvote service update

--- apps/redditApp/src/main/java/com/ScalableTeam/reddit/app/post/UpvotePostService.java
+++ apps/redditApp/src/main/java/com/ScalableTeam/reddit/app/post/UpvotePostService.java
@@ -1,5 +1,6 @@
 package com.ScalableTeam.reddit.app.post;
 
+import com.ScalableTeam.reddit.ICommand;
 import com.ScalableTeam.reddit.MyCommand;
 import com.ScalableTeam.reddit.app.entity.Post;
 import com.ScalableTeam.reddit.app.entity.vote.PostVote;
@@ -27,13 +28,13 @@ import java.util.Map;
 @Service
 @Slf4j
 @AllArgsConstructor
-public class UpvotePostService implements MyCommand {
+public class UpvotePostService implements ICommand<VotePostForm, String> {
     private final PostRepository postRepository;
     private final UserVotePostRepository userVotePostRepository;
     private final PostVoteRepository postVoteRepository;
     private final GeneralConfig generalConfig;
     private final PostVoteValidation postVoteValidation;
-//    @Value("${popularPostsUpvoteThreshold}")
+    //    @Value("${popularPostsUpvoteThreshold}")
 //    @Value("${popularPostsCache}")
 //    private String popularPostsCache;
 //    @Value("${postsCache}")
@@ -45,26 +46,20 @@ public class UpvotePostService implements MyCommand {
 
     @RabbitListener(queues = "${mq.queues.request.reddit.upvotePost}")
     public String execute(VotePostForm votePostForm, Message message) throws Exception {
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put("form", votePostForm);
-        attributes.put("message", message);
-        return (String) execute(attributes);
+        String indicator = generalConfig.getCommands().get("upvotePost");
+        String correlationId = message.getMessageProperties().getCorrelationId();
+        log.info(indicator + "Queue Listener::Vote Post Form={}, CorrelationId={}", votePostForm, correlationId);
+        return execute(votePostForm);
     }
 
     @Transactional(rollbackFor = {Exception.class})
     @Override
-    public Object execute(Object obj) throws Exception {
-        int popularPostsUpvoteThreshold=1;
-        Map<String, Object> attributes = (Map<String, Object>) obj;
-        VotePostForm votePostForm = (VotePostForm) attributes.get("form");
-        Message message = (Message) attributes.get("message");
-
+    public String execute(VotePostForm votePostForm) throws Exception {
+        int popularPostsUpvoteThreshold = 1;
         String userNameId = votePostForm.getUserNameId();
         String postId = votePostForm.getPostId();
-        String correlationId = message.getMessageProperties().getCorrelationId();
         String indicator = generalConfig.getCommands().get("upvotePost");
-        log.info(indicator + "Service::Post Id={}, User Id={}, CorrelationId={}", postId, userNameId, correlationId);
-
+        log.info(indicator + "Service::Post Id={}, User Id={}, CorrelationId={}", postId, userNameId);
 
         postVoteValidation.validatePostVote(userNameId, postId);
         String responseMessage = userVotePostRepository.upvotePost(userNameId, postId);
@@ -75,9 +70,9 @@ public class UpvotePostService implements MyCommand {
         post.setUpvoteCount(upvotesCount);
         post.setDownvoteCount(downvotesCount);
         postRepository.save(post);
-        if(upvotesCount>= popularPostsUpvoteThreshold){
+        if (upvotesCount >= popularPostsUpvoteThreshold) {
             System.err.println("here post");
-            cachingService.updatePopularPostsCache(postId,post);
+            cachingService.updatePopularPostsCache(postId, post);
         }
         if (cacheManager.getCache("postsCache").get(postId) != null) {
             cachingService.updatePostsCache(postId, post);
