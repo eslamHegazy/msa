da0bda0daf5b71342671d324ae2d957c48a28aa0
AhmedAbdouz <zewail102@gmail.com>
2022-05-29 01:04:37 +0200

    Added Tests for ReportUserCommand and solved some warnings

---
+++ apps/userApp/src/test/java/com/ScalableTeam/user/commands/ReportUserCommandTest.java
@@ -0,0 +1,72 @@
+package com.ScalableTeam.user.commands;
+
+import com.ScalableTeam.arango.User;
+import com.ScalableTeam.arango.UserRepository;
+import com.ScalableTeam.models.user.ReportUserBody;
+import com.ScalableTeam.models.user.ReportedUserResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public class ReportUserCommandTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ReportUserCommand reportUserCommand;
+
+    private ArrayList<String> savedUsers;
+
+    @BeforeEach
+    void setup() {
+        savedUsers = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            User user = User.builder().build();
+            user.setReportedUsers(new HashMap<>());
+            userRepository.save(user);
+            savedUsers.add(user.getUserNameId());
+        }
+    }
+
+    @Test
+    void userReportHimself(){
+        ReportUserBody body = new ReportUserBody(savedUsers.get(0),savedUsers.get(0),"dummy reason");
+        ReportedUserResponse response = reportUserCommand.execute(body);
+        assertFalse(response.isSuccessful());
+        assertEquals(response.getMessage(), "The user can not report himself/herself!");
+    }
+
+    @Test
+    void reportNonExistingUser(){
+        ReportUserBody body = new ReportUserBody(savedUsers.get(0),"0000","dummy reason");
+        ReportedUserResponse response = reportUserCommand.execute(body);
+        assertFalse(response.isSuccessful());
+        assertEquals(response.getMessage(), "The reported user not found in DB!");
+    }
+
+    @Test
+    void userNotFound(){
+        ReportUserBody body = new ReportUserBody("0000",savedUsers.get(0),"dummy reason");
+        ReportedUserResponse response = reportUserCommand.execute(body);
+        assertFalse(response.isSuccessful());
+        assertEquals(response.getMessage(), "User not found in DB!");
+    }
+
+    @Test
+    public void reportSuccessfully(){
+        ReportUserBody body = new ReportUserBody(savedUsers.get(1),savedUsers.get(0),"dummy reason");
+        ReportedUserResponse response = reportUserCommand.execute(body);
+        assertTrue(response.isSuccessful());
+        assertEquals(response.getMessage(), "User reported successfully");
+    }
+
+}
