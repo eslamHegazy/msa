2fe30a472eeeeb3a82023f70ea4690b05f551b56
Maria Maged <mariamaged988@gmail.com>
2022-05-28 05:34:03 +0200

    Commit initial version of thread controller

--- httpServer/src/main/java/com/ScalableTeam/server/app/ChatController.java
+++ httpServer/src/main/java/com/ScalableTeam/server/app/ChatController.java
@@ -1,9 +1,36 @@
 package com.ScalableTeam.server.app;
 
+import com.ScalableTeam.amqp.RabbitMQProducer;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.ScalableTeam.amqp.MessagePublisher.getMessageHeaders;
+
 @RestController
 @RequestMapping("/chat")
+@AllArgsConstructor
+@Slf4j
 public class ChatController {
+    private final RabbitMQProducer rabbitMQProducer;
+
+//    @PostMapping
+//    public void CreateGroupChat(@RequestBody Object post) throws Exception {
+//        String indicator = generalConfig.getCommands().get("createPost");
+//        log.info(indicator + "Controller", post);
+//        String commandName = "createPost";
+//        MessagePostProcessor messagePostProcessor = getMessageHeaders(
+//                config.getQueues().getResponse().getReddit().get(commandName));
+//        rabbitMQProducer.publishAsynchronous(
+//                post,
+//                config.getExchange(),
+//                config.getQueues().getRequest().getReddit().get("createPost"),
+//                messagePostProcessor);
+//        //return createPostService.execute(post);
+//    }
 }
---
+++ lib/src/main/java/com/ScalableTeam/Services/Controller/PropertiesManager.java
@@ -0,0 +1,74 @@
+package com.ScalableTeam.Services.Controller;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+
+@Component
+public class PropertiesManager {
+    private final Properties configProperties = new Properties();
+
+    public void loadProperties() throws IOException {
+        loadConfigProperties();
+    }
+
+    public void loadConfigProperties() throws IOException {
+        final InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.properties");
+        configProperties.load(inputStream);
+    }
+
+    private Integer readIntProperty(String propertyName) {
+        if (!configProperties.containsKey(propertyName))
+            return null;
+        return Integer.parseInt(configProperties.getProperty(propertyName));
+    }
+
+    private ArrayList<String> readArrayProperty(String propertyName, String delimiter) {
+        if (!configProperties.containsKey(propertyName))
+            return null;
+        return new ArrayList<>(Arrays.asList(configProperties.getProperty(propertyName).split(delimiter)));
+    }
+
+    public void updateProperty(String propertyName, String newValue) {
+        configProperties.replace(propertyName, newValue);
+    }
+
+//    public Map<String, DBConfig> getRequiredDbs() {
+//        final Map<String, DBConfig> requiredDbs = new HashMap<>();
+//        ArrayList<String> requiredDBsList = readArrayProperty(ServiceConstants.REQUIRED_DATABASES_PROPERTY_NAME,
+//                ServiceConstants.REQUIRED_DATABASES_ARRAY_DELIMITER,
+//                ServiceConstants.DEFAULT_REQUIRED_DATABASES);
+//
+//        for (String dbPair : requiredDBsList) {
+//            String[] split = dbPair.split(ServiceConstants.REQUIRED_DATABASES_PAIR_DELIMITER);
+//            String classPath = split[0];
+//            int connectionCount = Integer.parseInt(split[1]);
+//            requiredDbs.put(classPath, new DBConfig(connectionCount));
+//        }
+//
+//        System.out.println("Required Dbs: " + requiredDbs);
+//        return requiredDbs;
+//    }
+
+//    public int getControllerPort() {
+//        final InputStream stream = getClass().getClassLoader().getResourceAsStream("apps-ports.properties");
+//        final Properties properties = new Properties();
+//        try {
+//            properties.load(stream);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        String propertyName = appUriName.toLowerCase();
+//        return Integer.parseInt(properties.getProperty(propertyName));
+//
+//    }
+//
+//    public int getThreadCount() {
+//        return readIntProperty(ServiceConstants.THREADS_COUNT_PROPERTY_NAME, ServiceConstants.DEFAULT_THREADS_COUNT);
+//    }
+}
+    
