cfc344f617c5eba2d6977d4af0336cc8d394833e
MedoRedo <moreda.elsayed@gmail.com>
2022-05-28 11:10:06 +0200

    Added Unit tests

---
+++ apps/userApp/src/test/java/com/ScalableTeam/user/commands/DeleteAccountCommandTest.java
@@ -0,0 +1,58 @@
+package com.ScalableTeam.user.commands;
+
+import com.ScalableTeam.user.entity.UserProfile;
+import com.ScalableTeam.user.repositories.UserProfileRepository;
+import com.ScalableTeam.user.repositories.UserRepository;
+import models.DeleteAccountBody;
+import models.DeleteAccountResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doNothing;
+
+
+@DataJpaTest
+@ActiveProfiles("test")
+class DeleteAccountCommandTest {
+
+    String userId = "Mo99";
+    String email = "mo99@gmail.com";
+    String password = "pass";
+
+    @MockBean
+    UserRepository userRepository;
+    @Autowired
+    UserProfileRepository userProfileRepository;
+
+    DeleteAccountCommand deleteAccountCommand;
+    @BeforeEach
+    void setUp() {
+        userProfileRepository.save(new UserProfile(userId, email, password, null));
+        deleteAccountCommand = new DeleteAccountCommand(userProfileRepository, userRepository);
+    }
+
+    @Test
+    void deleteUsingWrongUsername(){
+        String wrongId = "Mo20";
+        doNothing().when(userRepository).deleteById(wrongId);
+        DeleteAccountBody deleteAccountBody = new DeleteAccountBody(wrongId);
+        DeleteAccountResponse deleteAccountResponse = deleteAccountCommand.execute(deleteAccountBody);
+        assertFalse(deleteAccountResponse.isSuccessful());
+        assertFalse(userProfileRepository.findAll().isEmpty());
+    }
+
+    @Test
+    void successfulDelete(){
+        doNothing().when(userRepository).deleteById(userId);
+        DeleteAccountBody deleteAccountBody = new DeleteAccountBody(userId);
+        DeleteAccountResponse deleteAccountResponse = deleteAccountCommand.execute(deleteAccountBody);
+        assertTrue(deleteAccountResponse.isSuccessful());
+        assertTrue(userProfileRepository.findAll().isEmpty());
+    }
+}
---
+++ apps/userApp/src/test/java/com/ScalableTeam/user/commands/LoginCommandTest.java
@@ -0,0 +1,63 @@
+package com.ScalableTeam.user.commands;
+
+import com.ScalableTeam.user.entity.UserProfile;
+import com.ScalableTeam.user.jwt.JwtUtil;
+import com.ScalableTeam.user.repositories.UserProfileRepository;
+import com.password4j.Hash;
+import com.password4j.Password;
+import models.LoginBody;
+import models.LoginResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class LoginCommandTest {
+
+    @Autowired
+    UserProfileRepository userProfileRepository;
+
+    String userId = "Mo99";
+    String email = "mo99@gmail.com";
+    String password = "pass";
+
+    static JwtUtil jwtUtil;
+
+    LoginCommand loginCommand;
+    @BeforeEach
+    void setUp() {
+        jwtUtil = new JwtUtil();
+        Hash hash = Password.hash(password).withBCrypt();
+        UserProfile userProfile = new UserProfile(userId, email, hash.getResult(), null);
+        userProfileRepository.save(userProfile);
+        loginCommand = new LoginCommand(userProfileRepository, jwtUtil);
+    }
+
+    @Test
+    void successfulLogin(){
+        LoginBody loginBody = new LoginBody(userId, password);
+        LoginResponse loginResponse = loginCommand.execute(loginBody);
+        assertTrue(loginResponse.isSuccessful());
+    }
+
+    @Test
+    void loginWithWrongUsername(){
+        LoginBody loginBody = new LoginBody("Mo98", password);
+        LoginResponse loginResponse = loginCommand.execute(loginBody);
+        assertFalse(loginResponse.isSuccessful());
+        assertEquals(loginResponse.getMessage(), "Wrong username");
+    }
+
+    @Test
+    void loginWithWrongPassword(){
+        LoginBody loginBody = new LoginBody(userId, "wrong pass");
+        LoginResponse loginResponse = loginCommand.execute(loginBody);
+        assertFalse(loginResponse.isSuccessful());
+        assertEquals(loginResponse.getMessage(), "Wrong username or password");
+    }
+}
