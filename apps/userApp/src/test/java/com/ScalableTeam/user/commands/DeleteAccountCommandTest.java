94ff2c26f4b29f6a5fb147d2d305857fdfefbdcd
MedoRedo <moreda.elsayed@gmail.com>
2022-05-28 20:01:05 +0200

    Fixed imports in Unit tests

--- apps/userApp/src/test/java/com/ScalableTeam/user/commands/DeleteAccountCommandTest.java
+++ apps/userApp/src/test/java/com/ScalableTeam/user/commands/DeleteAccountCommandTest.java
@@ -1,5 +1,8 @@
 package com.ScalableTeam.user.commands;
 
+import com.ScalableTeam.arango.UserRepository;
+import com.ScalableTeam.models.user.DeleteAccountBody;
+import com.ScalableTeam.models.user.DeleteAccountResponse;
 import com.ScalableTeam.user.entity.UserProfile;
 import com.ScalableTeam.user.repositories.UserProfileRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,38 +21,38 @@ import static org.mockito.Mockito.doNothing;
 @ActiveProfiles("test")
 class DeleteAccountCommandTest {
 
-//    String userId = "Mo99";
-//    String email = "mo99@gmail.com";
-//    String password = "pass";
-//
-//    @MockBean
-//    UserRepository userRepository;
-//    @Autowired
-//    UserProfileRepository userProfileRepository;
-//
-//    DeleteAccountCommand deleteAccountCommand;
-//    @BeforeEach
-//    void setUp() {
-//        userProfileRepository.save(new UserProfile(userId, email, password, null));
-//        deleteAccountCommand = new DeleteAccountCommand(userProfileRepository, userRepository);
-//    }
-//
-//    @Test
-//    void deleteUsingWrongUsername(){
-//        String wrongId = "Mo20";
-//        doNothing().when(userRepository).deleteById(wrongId);
-//        DeleteAccountBody deleteAccountBody = new DeleteAccountBody(wrongId);
-//        DeleteAccountResponse deleteAccountResponse = deleteAccountCommand.execute(deleteAccountBody);
-//        assertFalse(deleteAccountResponse.isSuccessful());
-//        assertFalse(userProfileRepository.findAll().isEmpty());
-//    }
-//
-//    @Test
-//    void successfulDelete(){
-//        doNothing().when(userRepository).deleteById(userId);
-//        DeleteAccountBody deleteAccountBody = new DeleteAccountBody(userId);
-//        DeleteAccountResponse deleteAccountResponse = deleteAccountCommand.execute(deleteAccountBody);
-//        assertTrue(deleteAccountResponse.isSuccessful());
-//        assertTrue(userProfileRepository.findAll().isEmpty());
-//    }
+    String userId = "Mo99";
+    String email = "mo99@gmail.com";
+    String password = "pass";
+
+    @MockBean
+    UserRepository userRepository;
+    @Autowired
+    UserProfileRepository userProfileRepository;
+
+    DeleteAccountCommand deleteAccountCommand;
+    @BeforeEach
+    void setUp() {
+        userProfileRepository.save(new UserProfile(userId, email, password, null));
+        deleteAccountCommand = new DeleteAccountCommand(userProfileRepository, userRepository);
+    }
+
+    @Test
+    void deleteUsingWrongUsername(){
+        String wrongId = "Mo20";
+        doNothing().when(userRepository).deleteById(wrongId);
+        DeleteAccountBody deleteAccountBody = new DeleteAccountBody(wrongId);
+        DeleteAccountResponse deleteAccountResponse = deleteAccountCommand.execute(deleteAccountBody);
+        assertFalse(deleteAccountResponse.isSuccessful());
+        assertFalse(userProfileRepository.findAll().isEmpty());
+    }
+
+    @Test
+    void successfulDelete(){
+        doNothing().when(userRepository).deleteById(userId);
+        DeleteAccountBody deleteAccountBody = new DeleteAccountBody(userId);
+        DeleteAccountResponse deleteAccountResponse = deleteAccountCommand.execute(deleteAccountBody);
+        assertTrue(deleteAccountResponse.isSuccessful());
+        assertTrue(userProfileRepository.findAll().isEmpty());
+    }
 }
