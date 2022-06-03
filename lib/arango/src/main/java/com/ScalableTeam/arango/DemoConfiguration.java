3cc9282bf5aa760b2e2dbe338bca61a22aec2550
SalmaElSayd <43332534+SalmaElSayd@users.noreply.github.com>
2022-06-03 21:54:51 +0200

    redditApp dockerized

--- lib/arango/src/main/java/com/ScalableTeam/arango/DemoConfiguration.java
+++ lib/arango/src/main/java/com/ScalableTeam/arango/DemoConfiguration.java
@@ -3,15 +3,18 @@ package com.ScalableTeam.arango;
 import com.arangodb.ArangoDB;
 import com.arangodb.springframework.annotation.EnableArangoRepositories;
 import com.arangodb.springframework.config.ArangoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableArangoRepositories(basePackages = {"com.ScalableTeam.arango", "com.ScalableTeam.user.repository", "com.ScalableTeam.reddit.app.repository"})
 public class DemoConfiguration implements ArangoConfiguration {
 
+    @Value("$")
+
     @Override
     public ArangoDB.Builder arango() {
-        return new ArangoDB.Builder().host("localhost", 8529).user("root").password(null);
+        return new ArangoDB.Builder().user("root").password(null);
     }
 
     @Override
