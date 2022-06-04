defa5a1e1d1ea33c47ed2dfc46d0f91e9f6b94c0
SalmaElSayd <43332534+SalmaElSayd@users.noreply.github.com>
2022-06-04 11:46:13 +0200

    fixed redis config host in redditApp

--- apps/redditApp/src/main/java/com/ScalableTeam/reddit/config/RedisConfig.java
+++ apps/redditApp/src/main/java/com/ScalableTeam/reddit/config/RedisConfig.java
@@ -1,10 +1,13 @@
 package com.ScalableTeam.reddit.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -12,7 +15,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 
 @Configuration
+@Slf4j
 public class RedisConfig {
+    private final String url;
+    private final int port;
+    public RedisConfig(@Value("${spring.redis.host}") String url,
+                       @Value("${spring.redis.port}") int port) {
+        this.url = url;
+        this.port = port;
+    }
     @Bean
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
         return (builder) -> builder
@@ -25,12 +36,16 @@ public class RedisConfig {
                 .withCacheConfiguration("recommendationsCache",
                         RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(7)));
     }
+    @Bean
+    public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+        log.info("Redis cache running on host: {}, port: {}", url, port);
+        return new RedisStandaloneConfiguration(url, port);
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisStandaloneConfiguration());
     }
-
     //Creating RedisTemplate for Entity 'Post'
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
