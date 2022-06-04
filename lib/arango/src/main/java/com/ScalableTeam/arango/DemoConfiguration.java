ea33dc4769d39247c1f76c6d6c6103f7e2c001e9
MedoRedo <moreda.elsayed@gmail.com>
2022-06-04 10:37:03 +0200

    Fix: ArangoDB host on application.properties in both reddit and user apps

--- lib/arango/src/main/java/com/ScalableTeam/arango/DemoConfiguration.java
+++ lib/arango/src/main/java/com/ScalableTeam/arango/DemoConfiguration.java
@@ -3,18 +3,22 @@ package com.ScalableTeam.arango;
 import com.arangodb.ArangoDB;
 import com.arangodb.springframework.annotation.EnableArangoRepositories;
 import com.arangodb.springframework.config.ArangoConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 @EnableArangoRepositories(basePackages = {"com.ScalableTeam.arango", "com.ScalableTeam.user.repository", "com.ScalableTeam.reddit.app.repository"})
 public class DemoConfiguration implements ArangoConfiguration {
 
     @Value("${arangodb.host}")
+    private String host;
 
     @Override
     public ArangoDB.Builder arango() {
-        return new ArangoDB.Builder().host("arangodb", 8529).user("root").password(null);
+        log.info("ArangoDB running on host: {}", host);
+        return new ArangoDB.Builder().host(host, 8529).user("root").password(null);
     }
 
     @Override
