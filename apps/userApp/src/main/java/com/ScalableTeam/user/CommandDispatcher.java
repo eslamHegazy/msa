ede0e4d376085305c351276ce04e5d08bc26dd86
MedoRedo <moreda.elsayed@gmail.com>
2022-06-04 03:24:28 +0200

    Update: redis cache host in properties files

--- apps/userApp/src/main/java/com/ScalableTeam/user/CommandDispatcher.java
+++ apps/userApp/src/main/java/com/ScalableTeam/user/CommandDispatcher.java
@@ -3,6 +3,7 @@ package com.ScalableTeam.user;
 import com.ScalableTeam.user.amqp.MessageConfig;
 import com.ScalableTeam.user.commands.ICommand;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.messaging.Message;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
+@Slf4j
 @AllArgsConstructor
 public class CommandDispatcher {
     private final Map<String, ICommand> commandMap;
@@ -19,14 +21,20 @@ public class CommandDispatcher {
 
     @RabbitListener(queues = MessageConfig.QUEUE, returnExceptions = "true")
     public Object receiveMessage(Message message, @Header("command") String command) {
-        ICommand iCommand = commandMap.get(command);
-        if (iCommand == null) {
-            iCommand = ctx.getBeansOfType(ICommand.class).get(command);
-        }
-        if (iCommand == null) {
-            System.out.println("Command does not exist");
+        log.info("Executing {}", command);
+        try {
+            ICommand iCommand = commandMap.get(command);
+            if (iCommand == null) {
+                iCommand = ctx.getBeansOfType(ICommand.class).get(command);
+            }
+            if (iCommand == null) {
+                System.out.println("Command does not exist");
+                return null;
+            }
+            return iCommand.execute(message.getPayload());
+        }catch (RuntimeException e){
+            log.error(e.getMessage());
             return null;
         }
-        return iCommand.execute(message.getPayload());
     }
 }
