4ee714327aa2b93fc902dccb64b5372520b5a350
MedoRedo <moreda.elsayed@gmail.com>
2022-06-04 10:08:15 +0200

    Fix: redis cache host and port configurations

--- apps/userApp/src/main/java/com/ScalableTeam/user/caching/RedisConfig.java
+++ apps/userApp/src/main/java/com/ScalableTeam/user/caching/RedisConfig.java
@@ -1,10 +1,13 @@
-package com.ScalableTeam.user;
+package com.ScalableTeam.user.caching;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -12,20 +15,26 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 
 @Configuration
+@Slf4j
 public class RedisConfig {
+    private final String url;
+    private final int port;
 
-    public static final String CACHE_NAME = "tokensCache";
+    public RedisConfig(@Value("${spring.redis.host}") String url,
+                                  @Value("${spring.redis.port}") int port) {
+        this.url = url;
+        this.port = port;
+    }
 
     @Bean
-    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
-        return (builder) -> builder
-                .withCacheConfiguration(CACHE_NAME,
-                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(30)));
+    public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+        log.info("Redis cache running on host: {}, port: {}", url, port);
+        return new RedisStandaloneConfiguration(url, port);
     }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisStandaloneConfiguration());
     }
 
     @Bean
